b91a176c9c9779d44d1a2e7bb7bf7ec0f7d0729a
dannae04 <113484198+dannae04@users.noreply.github.com>
2025-01-26 17:09:56 -0600

    Update datospersonales.php

--- LIS-CICLO2025/Guia1_GL210850/DiscusionResultados/datospersonales.php
+++ LIS-CICLO2025/Guia1_GL210850/DiscusionResultados/datospersonales.php
@@ -4,11 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
 </head>
+
 <body>
 <!--Realice un script PHP que muestre mediante la utilización de variables sus datos personales: nombre 
 completo, lugar de nacimiento (departamento y país, si es extranjero), edad y carnet de la universidad. 
 Muestre estos datos en una tabla.-->
+
+<style>
+table, th, td{
+border: 2px solid purple;
+border-collapse: collapse;
+background-color: #d3f3ff;
+font-family: Arial;
+}
+th, td {
+    padding: 10px;
+}
+th {
+    text-align: left;
+}
+h2 {
+    text-align: center;
+    font-size: 50px;
+    font-family: Arial;
+}
+</style>
+<h2>Datos Personales</h2>
 
 <?php
 $nombre_completo="Daniela Esmeralda Gutierrez";
@@ -16,7 +39,7 @@ $lugar_nacimiento="El Salvador";
 $edad="24 años";
 $carnet="GL210850";
 
-echo "<table border=1>";
+echo "<center><table border=1 width=500px>";
 echo "<tr>";
 echo "<td> Nombre Completo:</td>";
 echo "<td>" .$nombre_completo."</td>";
@@ -32,9 +55,10 @@ echo "<td> Carnet:";
 echo "<td>".$carnet."</td>";
 echo "</tr>";
 
-echo "</table>"
+echo "</table></center>"
 ?>
 
     
 </body>
+
 </html>
